7ed1504d80d19a45e7e4845e40e8d4acbffc1c83
Valentin Lab <valentin.lab@kalysto.org>
2015-01-30 11:51:26 +0700

    new: better prompt with help and validation regexp.
    It uses the ``context`` argument and interpret ``help``, ``default``,
    ``regex`` attributes.
    
    Example::
    
        context:
          name:
            help: "Module name (ie: sale_trinkets)"
            regex: '[a-zA-Z0-9_]+'
          website:
            help: "Website (ie: http://www.yourwebsite.com)"
            default:
            regex: '[^ ]+'
    
    Optional values and the ones having an empty default value.
    A value without default value is considered required.

--- cookiecutter/prompt.py
+++ cookiecutter/prompt.py
@@ -9,42 +9,109 @@ Functions for prompting the user for project info.
 """
 
 from __future__ import unicode_literals
-import sys
+from __future__ import print_function
 
-from .compat import iteritems, read_response, is_string
+import sys
+import re
+
+from .compat import read_response, is_string
 from jinja2.environment import Environment
 
 
-def prompt_for_config(context, no_input=False):
+def prompt_field(field, help_txt, value=None, regex=None,
+                 default=None, required=False):
+    """Prompt for value of one field
+
+    """
+
+    if default is not None:
+        prompt = '{0} (default is "{1}") ? '.format(help_txt, default)
+    elif not required:
+        prompt = '{0} (optional) ? '.format(help_txt)
+    else:
+        prompt = '{0} ? '.format(help_txt)
+
+    try:
+        reg = re.compile('^%s$' % regex) if regex else None
+    except re.error as e:
+        print("Invalid regex for field %s: %s\n%s"
+              % (field, regex, e.message))
+        exit(1)
+
+    while True:
+        if value:
+            ans = value
+        else:
+            ans = read_response(prompt).strip()
+        if ans == '':
+            if not required:
+                return None
+            if default is not None:
+                ans = default
+        if not reg:
+            return ans
+        if reg.search(ans):
+            return ans
+
+        print("Bad value for template field '%s'. Please try to match %r..."
+              % (field, regex))
+        value = None
+
+
+def prompt_for_config(context, no_input=False, only_missing=None, values=None,
+                      with_optional=True):
     """
     Prompts the user to enter new config, using context as a source for the
     field names and sample values.
 
     :param no_input: Prompt the user at command line for manual configuration?
     """
-
     if "cookiecutter" in context:
         context = context["cookiecutter"]
+
+    if values is None:
+        values = {}
 
     cookiecutter_dict = {}
     env = Environment()
 
-    for key, raw in iteritems(context):
+    def render(s):
+        return env.from_string(s).render(
+            cookiecutter=cookiecutter_dict,
+            cc=cookiecutter_dict)
+
+    meta = context.get('__meta__', {})
+    if only_missing is None:
+        only_missing = True if meta else False
+
+    keys = meta.keys() if meta else context.keys()
+    for key in keys:
+        dsc = meta.get(key, {})
+        raw = values.get(key, None) or context.get(key, None)
         if key.startswith('_'):
             cookiecutter_dict[key] = raw
             continue
-        raw = raw if is_string(raw) else str(raw)
-        val = env.from_string(raw).render(cookiecutter=cookiecutter_dict)
+        if raw is not None:
+            raw = raw if is_string(raw) else str(raw)
+        else:
+            raw = dsc.get("default", None)
+        default = render(raw) if raw is not None else None
 
-        if not no_input:
-            prompt = '{0} (default is "{1}")? '.format(key, val)
+        optional = dsc and "default" in dsc and dsc["default"] is None
 
-            new_val = read_response(prompt).strip()
+        if no_input or \
+           (only_missing and default is not None) or \
+           (optional and not with_optional):
+            value = default
+        else:
+            value = prompt_field(
+                field=key, value=values.get(key, None),
+                help_txt=render(dsc["help"]) if dsc and "help" in dsc else key,
+                default=default,
+                required=not optional,
+                regex=dsc["regex"] if dsc and "regex" in dsc else None)
 
-            if new_val != '':
-                val = new_val
-
-        cookiecutter_dict[key] = val
+        cookiecutter_dict[key] = value
     return cookiecutter_dict
 
 
